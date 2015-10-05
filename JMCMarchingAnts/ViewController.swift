b3a7af2cc4a154a45877ede046a712ab2f322604
Janusz Chudzynski <appzzman>
2015-10-05 10:52:32 -0500

    Initial Commit

---
+++ JMCMarchingAnts/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JMCMarchingAnts
+//
+//  Created by Janusz Chudzynski on 10/5/15.
+//  Copyright (c) 2015 Janusz Chudzynski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
